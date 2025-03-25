6a436089dff02a4274a328bffdb8fb4799d9b24d
Sakshi Ghodake <157048909+Ghodake123@users.noreply.github.com>
2025-03-25 23:28:10 +0530

    failed test cases

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -26,6 +26,11 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone), containsString(someone));
   }
+   @Test
+  public void greetShouldIncludeTheOneBeingGreeted() {
+   assertTrue[false];
+  }
+  
   
 
   @Test
